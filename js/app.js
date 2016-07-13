d0b9fc9ee77a13faa00f47ff3fe4b68cc0bab061
janagan1493 <janagan1493@gmail.com>
2016-07-13 18:44:13 -0400

    Commit js/app.js

--- js/app.js
+++ js/app.js
@@ -1,56 +1,71 @@
 
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
+
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
-  	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	/*--- Start a New Game ---*/
-
 	$(".button").on("click", function() {
 		event.preventDefault();
 		var userGuess = $("input").val();
-		validateGuess(userGuess); 
+		playFeedback(userGuess); 
 	});
 
 	$(".new").on("click", function() {
-		
+	
 	});
 
 	var randomNumber = Math.floor(Math.random() * 100) + 1;
 
-	function playGame(userGuess) {
+	function playFeedback(userGuess) {
 
 		if (userGuess > randomNumber) {
-			$("#feedback").text('You guessed too high!');
+			$("#feedback").text('You are too high!');
 			$("span").html(parseInt($("span").html()) + 1);
 		}
-
 		else if (userGuess < randomNumber) {
-			$("#feedback").text('You guessed too low!');
+			$("#feedback").text('You are too low!');
 			$("span").html(parseInt($("span").html()) + 1);
 		}
-
 		else {
-			$("#feedback").text('You guessed correctly!');
+			$("#feedback").text('Congratulations, you win!');
 			$("span").html(parseInt($("span").html()) + 1);
-		}	
-	
+		}
 	}
 
 	function validateGuess(userGuess) {
 		if (userGuess % 1 === 0 && userGuess >= 1 && userGuess <= 100) {
-			playGame(userGuess);
+			playFeedback(userGuess);
+			sendGuestList(userGuess);
 		}
 		else {
 			$("#feedback").text('ERROR: Number must be an integer between 0 and 100!');
+			$("#guessList").text('Please try again!');
+		}
+	}
+
+	function sendGuestList(userGuess) {
+		if (userGuess === randomNumber) {
+			$("#guessList").text('You are burning up!');
+		}
+		else if (userGuess === randomNumber + 50 <= 100) {
+			$("#guessList").text('You are ice cold!');
+		}
+		else if (userGuess === randomNumber + 30 <= 49) {
+			$("#guessList").text('You are cold!');
+		}
+		else if (userGuess === randomNumber + 20 <= 29) {
+			$("#guessList").text('You are warm!');
+		}
+		else if (userGuess === randomNumber + 11 <= 19) {
+			$("#guessList").text('You are hot!');
+		}
+		else {
+			$("#guessList").text('You are on fire!');
 		}
 	}
 
